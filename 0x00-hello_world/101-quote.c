b8e1975b4a979c3d39b998ee637907c5c40d4dd0
SEKiZ-96 <118661365+SEKiZ-96@users.noreply.github.com>
2023-06-16 00:13:05 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,8 @@
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                followed by a new line, to standard error.
- *
+ * main - Entry point
+ * 
  * Return: Always 1.
  */
 int main(void)
