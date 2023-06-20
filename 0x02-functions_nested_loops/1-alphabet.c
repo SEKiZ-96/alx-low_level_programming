4d4a95cd054fc5e2a90228c135a7a3b337d436a3
Anas <anassadil17@gmail.com>
2023-06-20 10:02:39 -0700

    C is good

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: function print alphabet in lowercase
+ *
+ */
 void print_alphabet(void)
 {
-    char letter;
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        _putchar(letter);
-    }
-    _putchar('\n');
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
 }
