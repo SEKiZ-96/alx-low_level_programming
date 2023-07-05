96620bb377b30700971f4cc94d2de9b616f4d1a0
Anas <anassadil17@gmail.com>
2023-07-05 14:39:26 -0700

    C

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
+julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-main.c 0-puts_recursion.c -o 0-puts_recursion
+julien@ubuntu:~/0x08. Recursion$ ./0-puts_recursion 
+Puts with recursion
+julien@ubuntu:~/0x08. Recursion$ 
